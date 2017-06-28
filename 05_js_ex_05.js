5cc158929b4697cc7b55a9d21c2edd408685a671
dkrwlq <dkrwlq@naver.com>
2017-06-28 18:18:58 +0900

    자바스크립트

---
+++ 05_js_ex_05.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017-06-28.
+ */
+
+var guk = 90;
+var young = 90;
+
+if(( guk >= 90 && young >= 90)){
+
+  console.log('수');
+
+} else if (( guk >= 90 && young >= 80)){
+
+  console.log('우');
+
+} else if (( guk >= 80 && young >= 80)){
+
+  console.log('미');
+
+} else {
+
+  console.log('낙제');
+
+}
